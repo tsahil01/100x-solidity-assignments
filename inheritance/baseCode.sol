0958727e84cf81c05311122f492092393eb32f08
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-12-14 23:55:13 +0530

    inhertance code done

--- inheritance/baseCode.sol
+++ inheritance/baseCode.sol
@@ -3,8 +3,16 @@ pragma solidity ^0.8.0;
 
 contract Parent {
 
+    function greet() public pure virtual returns (string memory) {
+        return "Hello from Parent";
+    }
+
 }
 
-contract Child{
+contract Child is Parent{
+
+    function greet() public pure override returns (string memory) {
+        return "Hello from Parent";
+    }
     
 }
